b6daabf1f1a966f8548af24dd0e897b9deb6994b
Joshua <your-github-email@example.com>
2026-02-06 20:36:13 +0000

    Apply .gitignore and clean repo

--- recoil-todo/src/App.jsx
+++ recoil-todo/src/App.jsx
@@ -1,14 +1,3 @@
-import TodoInput from "./components/TodoInput";
-import TodoFilters from "./components/TodoFilters";
-import TodoList from "./components/TodoList";
-
 export default function App() {
-  return (
-    <div className="container">
-      <h1>📝 Recoil To-Do List</h1>
-      <TodoInput />
-      <TodoFilters />
-      <TodoList />
-    </div>
-  );
+  return <h1>App is rendering ✅</h1>;
 }
